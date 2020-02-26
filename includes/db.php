1aab5ebe5cb0cac5390be842a95e02468179bc75
Arly0 <mrsmileball@gmail.com>
2020-02-26 17:12:16 +0100

    first review

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,24 @@
 <?php
-try //connect to db
-{
-$connect = new PDO('mysql:host=127.0.0.1;dbname=test_db', 'root', '');
-$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}//catch errors
-catch(PDOException $e)
-{
-echo 'ERROR: ' . $e->getMessage();
-exit();
-}
+/*
+ * 1)сразу бросается в глаза - используй табуляцию. код не читается. всё должно быть структуризировано как в HTML тегах
+ * 2) host, dbname, root, password лучше вынести в отдельные переменные и отделить их ентерами чтобы лучше читалосьи было понятно,
+ * что это настройки
+ * 3) лишни и слишком очевидные комментарии не нужны. и так понятно по названию файла, что это файл для БД
+ */
+
+    $host = '127.0.0.1';
+    $dbName = 'test_db';
+    $user = 'root';
+    $password = '';
+
+    try
+    {
+        // обрати внимание, что можно юзать такой синтаксис :)
+        $connect = new PDO("mysql:host={$host};dbname={$dbName}", $user, $password);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo 'ERROR: ' . $e->getMessage();
+        exit();
+    }
