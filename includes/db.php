6fba53f58c258e71b861716ac5694da8c105511b
Viktor <46136749+toravb@users.noreply.github.com>
2020-02-26 16:43:23 +0200

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,11 @@
-
+<?php
+try //connect to db
+{
+$connect = new PDO('mysql:host=127.0.0.1;dbname=test_db', 'root', '');
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}//catch errors
+catch(PDOException $e)
+{
+echo 'ERROR: ' . $e->getMessage();
+exit();
+}
